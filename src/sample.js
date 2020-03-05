f87cb475b39b18a9ec79e492cfad4524776915f3
PB-fushino <teppei.fushino@plan-b.co.jp>
2020-03-06 01:46:10 +0900

    sqs

--- src/sample.js
+++ src/sample.js
@@ -3,18 +3,21 @@ const AWSXRay = require('aws-xray-sdk');
 // aws-sdkはLambdaのランタイムにのっているので、devDependenciesでOK
 const AWS = AWSXRay.captureAWS(require('aws-sdk')); // eslint-disable-line
 
-const sqs = new AWS.SQS({ region: 'us-west-2' });
+const sqs = new AWS.SQS({ region: 'ap-northeast-1' });
 
-// 関数のサンプル
+// 関数のサンプル非同期処理
 exports.handler = async (event, context) => {
   const { key1 } = event;
+  console.log(event);
   // 環境変数を使用するサンプル
-  const sample = process.env.ENV_SAMPLE;
+  const sample = process.env.ENV_SAMPLE;//resoleならlistを取得する,err
   const queues = new Promise((resolve, reject) => sqs.listQueues({}, (err, data) => {
     if (err) reject(err);
+    console.log(error);
     resolve(data);
   })).catch((reason) => context.fail(reason));
   const result = `key1 = ${key1}, 環境変数 = ${sample}`;
+  //key1 = 'event , 環境変数 = dev';
   context.succeed({
     result,
     queues,
